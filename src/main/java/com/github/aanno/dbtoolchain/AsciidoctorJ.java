036ca9671221af9466abea7d79c9d932c993f6f0
Thomas Pasch <thomas.pasch@gmx.de>
2018-12-16 17:32:35 +0100

    fixes

--- src/main/java/com/github/aanno/dbtoolchain/AsciidoctorJ.java
+++ src/main/java/com/github/aanno/dbtoolchain/AsciidoctorJ.java
@@ -3,8 +3,10 @@ package com.github.aanno.dbtoolchain;
 import static org.asciidoctor.Asciidoctor.Factory.create;
 import static org.asciidoctor.OptionsBuilder.options;
 import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Options;
 import org.asciidoctor.SafeMode;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
@@ -21,10 +23,13 @@ public class AsciidoctorJ {
     private Map<String, Object> options;
 
     public AsciidoctorJ() {
-options = options().safe(SafeMode.SAFE).asMap();
-options.put("backend", "docbook5");
+options = options().safe(SafeMode.SAFE).toFile(new File("out.fo.xml")).asMap();
+options.put(Options.BACKEND, "docbook5");
     }
 
+    /**
+     * TODO tp: no doc type...
+     */
     public void convert(Path in, Path out) throws IOException {
         try (Reader reader = Files.newBufferedReader(in);
         Writer writer = Files.newBufferedWriter(out)) {
@@ -32,12 +37,21 @@ options.put("backend", "docbook5");
         }
     }
 
+    public void convert2(Path in, Path out) throws IOException {
+        Map<String, Object> opts = new HashMap<>(options);
+        // opts.put(Options.TO_FILE, "false");
+        // opts.put(Options.TO_DIR, ".");
+        // opts.put(Options.BASEDIR, ".");
+        String result = asciidoctor.convertFile(in.toFile(), options);
+        System.out.println(result);
+    }
+
     public static void main(String[] args) throws Exception {
         Path test = Paths.get("asciidoctor.org/README.adoc");
         Path out = Paths.get("out.fo.xml");
 
         AsciidoctorJ adj = new AsciidoctorJ();
-        adj.convert(test, out);
+        adj.convert2(test, out);
         System.out.println("asciidoctorj from " + test + " to " + out);
     }
 
