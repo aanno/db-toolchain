74f0d9fb0369e73b524ddaca653338a24a50d3ae
Thomas Pasch <thomas.pasch@gmx.de>
2019-01-16 11:45:10 +0100

    fo document as resource

--- src/main/java/com/github/aanno/dbtoolchain/BarcodeExample.java
+++ src/main/java/com/github/aanno/dbtoolchain/BarcodeExample.java
@@ -9,6 +9,11 @@ public class BarcodeExample {
     private static final XmlSingleton XML_SINGLETON = XmlSingleton.getInstance();
 
     public static void main(String[] args) throws Exception {
-        org.apache.fop.cli.Main.main("out.fo.xml -pdf out.pdf".split("[ \t]"));
+        /*
+        org.apache.fop.cli.Main.main(
+                "src/test/resources/fo/complete_document.fo.xml -pdf out.pdf".split("[ \t]"));
+                */
+        org.apache.fop.cli.Main.main(
+                "src/test/resources/fo/complete_document_barcode.fo.xml -pdf barcode.pdf".split("[ \t]"));
     }
 }
